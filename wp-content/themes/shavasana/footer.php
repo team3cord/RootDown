2ed8cd808ee74d4e59253430fbc1d0eafded21a1
MattCordeiro <matthew3cord@gmail.com>
2013-10-25 21:04:34 -0700

    Initial commit

---
+++ wp-content/themes/shavasana/footer.php
@@ -0,0 +1,80 @@
+<?php
+/**
+
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+?>
+
+            <!-- ============================= | GLOBAL FOOTER | ============================= -->
+        <footer class="gFooter">
+        
+            <!-- Top Footer Section -->
+                <section class="mid">
+
+                        <h1>Root Down Yoga | Power for the gorge | Hood River Oregon</h1>
+                        <a href="#" class="footUtility fUnews">Get our newsletter</a>
+                        <a href="#" class="footUtility fUclass">Sign up for a class</a>
+                    
+                    <!-- Footer Logo Wrap | only shown on mobile -->
+                    <div class="footLogo mOnly">
+<!--                         <h1>Root Down</h1> -->
+                    </div>
+                    <!-- END FOOTER LOGO WRAP -->
+
+
+                </section>
+
+            <!-- Bottom Footer Section -->
+            <section class="full footContact">
+                <section class="mid">
+                    <div class="footLegal">
+                        <span>© 2013 VinYoga</span>
+                        <span class="footLocation">Hood River Oregon</span>
+                        <span>541.399.4683</span>
+                    </div>
+
+                    <nav class="footNav">
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Classes</a></li>
+                            <li><a href="#">Workshops</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </nav>
+
+                    <ul class="footSocial">
+                        <li><a href="#" class="sIcons facebook"><i class="icon-facebook"></i></li>
+                        <li><a href="#" class="sIcons instagram"><i class="icon-instagram"></i></a></li>
+                        <li><a href="#" class="sIcons pintrest"><i class="icon-pinterest"></i></a></li>
+                        <li><a href="#" class="sIcons twitter"><i class="icon-twitter"></i></a></li>
+                    </ul>
+                </section>
+            </section>
+            <!-- END BOTTOM FOOTER SECTION -->
+            
+        </footer>
+        <!-- =========================== | END GLOBAL FOOTER | =========================== -->
+
+
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+
+    <!-- MENU SCRIPS -->
+    <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/hammer.js/1.0.5/jquery.hammer.min.js"></script>
+    <script type="text/javascript" src="js/mmenu.min.js"></script>
+
+    <!-- MENU SCRIP -->
+    <script type="text/javascript">
+        $(function() {
+            $('nav#menu').menu({
+                dragOpen: true
+            });
+        });
+    </script>
+
+
+</body>
+</html>
---
+++ wp-content/themes/shavasana/index.php
@@ -0,0 +1,124 @@
+<?php
+/**
+
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+
+get_header(); ?>
+
+
+        <!-- =========================== | GLOBAL HERO WRAP | =========================== -->
+        <section class="gHeroWrap homeHero fullWrap">
+
+                <!-- Global Hero | Main image / video / slider after header -->
+                <section class="gHero full">
+                    <div><img src="http://placehold.it/1200x550&text=Image / Video / Slider"></div>
+                </section>
+                <!-- !!!END GLOBAL HERO -->
+
+        </section>
+        <!-- ========================= | !!!END GLOBAL HERO WRAP | ========================= -->
+
+
+
+        <!-- =========================== | CONTENT WRAP | =========================== -->
+        <div class="gContent">
+
+            <!-- =========================== |  | =========================== -->
+            <!-- =========================== | !!!END | =========================== -->
+
+            <!-- =========================== | Featured tout wrap | =========================== -->
+            <section class="ftw full">
+                <!-- ad Wrap | wraps specials ad -->
+                <div class="adWrap nonM half">
+                    <p>Place Ad Here</p> 
+                </div>
+                 <!-- schedule wrap Wrap | wraps  schedule-->
+                <div class="shedWrap">
+                    <h2>Date is here</h2>
+                    <div class="mbWrap">
+                        This is the mind body stuff
+                    </div>
+                    <a href="#" class="btn btnPurp Share"><i class="icon-plus"></i>Share</a>
+                    <a href="#" class="btn">Learn More</a>
+                    <a href="#" class="btn">Sign Up</a>
+                </div>
+            </section>
+            <!-- =========================== | !!!END FTW | =========================== -->
+
+            <!-- =========================== |  | =========================== -->
+            <section class="iAbout full">
+                <div class="iAboutText">
+                    <h2>About RootDownYoga</h2>
+                    <p>We at Root Down Yoga believe in living a grounded, simple life filled with vitality and health.  Our goal is to offer a simple, clean, and quiet space with invigorating, fun classes designed to suit the needs of all our members.</p>
+                    <a href="#" class="btn">Learn More</a>
+                    <div>
+                        <h2>News Letter Sign Up</h2>
+                         <form action="index.html" method="post" class="subscribe-form">
+                          <input type="email" name="email" class="subscribe-input" placeholder="Email address" autofocus>
+                          <button type="submit" class="subscribe-submit">Subscribe</button>
+                        </form>
+                    </div>
+                </div>
+                <div class="iAboutClass">
+                    <h2>Yoga Classes</h2>
+                    <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. get lacinia odio sem nec elit.</p>
+                    <a href="#" class="btn">Learn More</a>
+                <div>
+            </section>
+            <!-- =========================== | !!!END | =========================== -->
+            
+
+            <!-- =========================== |  | =========================== -->
+            <section class="iYogaAk fulll">
+                
+                <div class="iYogaKid">
+                    <h2>Yoga for Kids</h2>
+                    <p>Maecenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus. Maenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna.</p>
+                    <a href="#" class="btn">Learn More</a>
+                    <a href="#" class="btn">View Schedule</a>
+                </div>
+
+                <div  class="iYogaAdult">
+                    <h2>Yoga for Adults</h2>
+                    <p>Maecenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus. Maenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna.</p>
+                    <a href="#" class="btn">Learn More</a>
+                    <a href="#" class="btn">View Schedule</a>
+                </div>
+
+                <section class="snapG">
+                    <p>Snap Goes Here</p>
+                </section>
+
+                <section class="iBlogWrap">
+                    <img height="180" width="385" style="padding-top:27px;border-width:0" onload="window.lol&amp;&amp;lol()" id="hplogo" src="/logos/doodles/2013/celia-cruzs-88th-birthday-5720827718795264-hp.jpg" alt="Celia Cruz's 88th Birthday" title="Celia Cruz's 88th Birthday">
+                    <h2>Post Name Here</h2>
+                    <p>Maecenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus. Maenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna.</p>
+                    <a href="#" class="btn">Learn More</a>
+                    <a href="#" class="btnPurp Share">Share</a>
+                </section>
+
+                <section class="iInstaWrap">
+                    <h1>Power Yoga for the Gorge</h1>
+                    <div> 
+                        igram imgs here
+                    </div>
+                </section>
+
+            </section>
+            <!-- =========================== | !!!END | =========================== -->
+
+
+
+        <div>
+        <!-- =========================== | !!!END CONTENT WRAP | =========================== -->
+
+
+    </div>
+    <!-- =========================== | !!!END GLOBAL PAGE WRAP | =========================== -->
+ </div>
+
+
+<?php get_footer(); ?>
