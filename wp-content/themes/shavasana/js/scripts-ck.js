921e8de2d25cf4fd5b6c41cc8ec49f78d381eeb0
stauss <caleb@Administrators-MacBook-Pro.local>
2013-10-28 08:41:29 -0700

    css changes and some html changes

---
+++ wp-content/themes/shavasana/js/scripts-ck.js
@@ -0,0 +1,2 @@
+// DOM Ready
+$(function(){if(!Modernizr.svg){var e=document.getElementsByTagName("img"),t=/.*\.svg$/;for(var n=0;n!=e.length;++n)e[n].src.match(t)&&(e[n].src=e[n].src.slice(0,-3)+"png")}});
