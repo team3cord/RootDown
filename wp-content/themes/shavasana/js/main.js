0195dc5ffbb98d4b46870f8211157f1708ec2f2f
stauss <caleb@Administrators-MacBook-Pro.local>
2013-10-29 19:15:23 -0700

    css changes html changes and image changes

--- wp-content/themes/shavasana/js/main.js
+++ wp-content/themes/shavasana/js/main.js
@@ -3,6 +3,29 @@ $(function() {
         dragOpen: true
     });
 
-    alert('awesome');
+
+    // Teacher Scripts
+
+    var links = $("#teacherNav li"),
+    	teacherWrap = $('.teacher').each(function(){}),
+    	teacherImg = $('.teacherImgs').each(function(){});
+
+
+
+    links.on('click', function(e){
+
+        e.preventDefault(); // stop the browser from jumping
+
+    	var i = $(this).index(),                   // index used to pass into array
+    		nextTecher = teacherWrap[i].id,        // on this # array get the id
+    		nextImg = teacherImg[i].id;
+
+    		teacherWrap.addClass('notActive'); 	   // hide current teacher
+    		$("#" + nextTecher).removeClass('notActive');   // show next teaher
+
+    		teacherImg.addClass('notActive'); 	   // hide current teacher
+    		$("#" + nextImg).removeClass('notActive');   // show next teaher
+
+    });
 
 });
