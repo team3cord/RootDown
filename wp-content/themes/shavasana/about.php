f1c41429ce871876a1fd16e86788b0f1b9c510f4
MattCordeiro <matthew3cord@gmail.com>
2013-10-29 20:47:51 -0700

    Added to templates

--- wp-content/themes/shavasana/about.php
+++ wp-content/themes/shavasana/about.php
@@ -6,133 +6,161 @@ Template Name: ABOUT Page
 get_header(); ?>
 
 
-    <!-- =========================== | GLOBAL HERO WRAP | =========================== -->
-    <section class="gHeroWrap aboutHero fullWrap">
-        <div>
-            <h2>About Root Down</h2>
-            <p>this is a paragraph</p>
+<!-- =========================== | GLOBAL HERO WRAP | =========================== -->
+<section class="gHeroWrap blogHero fullWrap">
+    <div>
+        <h2>About Root Down Yoga</h2>
+        <p>Curabitur blandit tempus porttitor. Donec ullamcorper nulla non metus auctor fringilla. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec id elit non mi porta gravida at eget metus.</p>
+    </div>
+</section>
+<!-- ========================= | !!!END GLOBAL HERO WRAP | ========================= -->
+
+
+
+<!-- =========================== | CONTENT WRAP | =========================== -->
+<div class="gContent">
+    <!-- =========================== |  | =========================== -->
+    <!-- =========================== | !!!END | =========================== -->
+
+    <!-- =========================== | Featured tout wrap | =========================== -->
+    <section class="ftw full">
+        <!-- ad Wrap | wraps specials ad -->
+        <div class="adWrap nonM half">
+            <p>Place Ad Here</p>
+        </div>
+        <!-- schedule wrap Wrap | wraps  schedule-->
+        <div class="shedWrap">
+            <h2>Date is here</h2>
+            <div class="mbWrap">
+                This is the mind body stuff
+            </div>
+            <a href="#" class="btn btnPurp Share"><i class="icon-plus"></i>Share</a>
+            <a href="#" class="btn">Learn More</a>
+            <a href="#" class="btn">Sign Up</a>
         </div>
     </section>
-    <!-- ========================= | !!!END GLOBAL HERO WRAP | ========================= -->
+    <!-- =========================== | !!!END FTW | =========================== -->
 
 
+    <section class="teacherWrap">
 
-    <!-- =========================== | CONTENT WRAP | =========================== -->
-    <div class="gContent">
-        <!-- =========================== |  | =========================== -->
-        <!-- =========================== | !!!END | =========================== -->
+        <div class="teacherInner">
 
-        <!-- =========================== | Featured tout wrap | =========================== -->
-        <section class="ftw full">
-            <!-- ad Wrap | wraps specials ad -->
-            <div class="adWrap nonM half">
-                <p>Place Ad Here</p>
+            <div class="techerTitleWrap">
+                <h1>About Our Teachers</h1>
             </div>
-            <!-- schedule wrap Wrap | wraps  schedule-->
-            <div class="shedWrap">
-                <h2>Date is here</h2>
-                <div class="mbWrap">
-                    This is the mind body stuff
-                </div>
-                <a href="#" class="btn btnPurp Share"><i class="icon-plus"></i>Share</a>
-                <a href="#" class="btn">Learn More</a>
-                <a href="#" class="btn">Sign Up</a>
-            </div>
-        </section>
-        <!-- =========================== | !!!END FTW | =========================== -->
 
+            <div class="teacherCustomPic">
 
-        <section class="teacherWrap">
-
-            <div class="teacherInner">
-
-                <div class="techerTitleWrap">
-                    <h1>About Our Teachers</h1>
+                <div id="techAngela" class="teacherImgs">
+                    <img src="img/theacherPics/startAngela.png" class="startImg">
+                    <img src="img/theacherPics/startAngela.png" class="nonActiveImg">
+                    <img src="img/theacherPics/startAngela.png" class="nonActiveImg">
                 </div>
 
-                <nav class="teacherNav">
-                    <ul>
-                        <li><a href="#">Angela</a></li>
-                        <li><a href="#">Victoria</a></li>
-                        <li><a href="#">Chelsey</a></li>
-                        <li><a href="#">elanor</a></li>
-                    </ul>
-                </nav>
-
-                <div class="teacherPicNav">
-                    <div class="tpnPopUp">this is a pop up</div>
-                    <a href="#" class="tpvLinkLeft">Left side</a>
-                    <a href="#" class="tpvLinkRight">Right side</a>
+                <div id="techVic" class="teacherImgs notActive">
+                    <img src="img/theacherPics/startVic.png" class="startImg">
+                    <img src="img/theacherPics/startAngela.png" class="nonActiveImg">
+                    <img src="img/theacherPics/startAngela.png" class="nonActiveImg">
                 </div>
 
-                <!-- =========================== | Angela | =========================== -->
-                <section class="angelaW teacher">
-                    <div class="teacherTextWrap">
-                        <img src="http://placehold.it/350x150" class="mobileTeccherImg">
-                        <div class="ttwInner">
-                            <h2>Angela Logan</h2>
-                            <p>Maecenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus. Maenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna.</p>
-                            <a href="#" class="btn">Learn More</a>
-                            <a href="#" class="btn btnPurp Share"><i class="icon-plus"></i>Share</a>
-                        </div>
-                    </div>
-                </section>
-                <!-- =========================== | !!!END Angela| =========================== -->
+                <div id="techChels" class="teacherImgs notActive">
+                    <img src="img/theacherPics/startChels.png" class="startImg">
+                    <img src="img/theacherPics/startAngela.png" class="nonActiveImg">
+                    <img src="img/theacherPics/startAngela.png" class="nonActiveImg">
+                </div>
 
-                <!-- =========================== | Angela | =========================== -->
-                <section class="victoriaW teacher notActive">
-                    <div class="teacherTextWrap">
-                        <img src="http://placehold.it/350x150" class="mobileTeccherImg">
-                        <div class="ttwInner">
-                            <h2>Angela Logan</h2>
-                            <p>Maecenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus. Maenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna.</p>
-                            <a href="#" class="btn">Learn More</a>
-                            <a href="#" class="btn btnPurp Share"><i class="icon-plus"></i>Share</a>
-                        </div>
-                    </div>
-                </section>
-                <!-- =========================== | !!!END Angela| =========================== -->
+                <div id="techEl" class="teacherImgs notActive">
+                    <img src="img/theacherPics/startEl.png" class="startImg">
+                    <img src="img/theacherPics/startAngela.png" class="nonActiveImg">
+                    <img src="img/theacherPics/startAngela.png" class="nonActiveImg">
+                </div>
 
-                <!-- =========================== | Angela | =========================== -->
-                <section class="chelseyW teacher notActive">
-                    <div class="teacherTextWrap">
-                        <img src="http://placehold.it/350x150" class="mobileTeccherImg">
-                        <div class="ttwInner">
-                            <h2>Angela Logan</h2>
-                            <p>Maecenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus. Maenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna.</p>
-                            <a href="#" class="btn">Learn More</a>
-                            <a href="#" class="btn btnPurp Share"><i class="icon-plus"></i>Share</a>
-                        </div>
-                    </div>
-                </section>
-                <!-- =========================== | !!!END Angela| =========================== -->
-
-                <!-- =========================== | Angela | =========================== -->
-                <section class="elanorW teacher notActive">
-                    <div class="teacherTextWrap">
-                        <img src="http://placehold.it/350x150" class="mobileTeccherImg">
-                        <div class="ttwInner">
-                            <h2>Angela Logan</h2>
-                            <p>Maecenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus. Maenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna.</p>
-                            <a href="#" class="btn">Learn More</a>
-                            <a href="#" class="btn btnPurp Share"><i class="icon-plus"></i>Share</a>
-                        </div>
-                    </div>
-                </section>
-                <!-- =========================== | !!!END Angela| =========================== -->
             </div>
-        </section>
-        <!-- =========================== | !!!END  Teacher Wrap| =========================== -->
 
+            <nav class="teacherNav" id="teacherNav">
+                <ul>
+                    <li><a href="#" id="angelaLink" data-teacher="angela">Angela</a></li>
+                    <li><a href="#" id="vicLink" data-teacher="vic">Victoria</a></li>
+                    <li><a href="#" id="chelsLink" data-teacher="chels">Chelsey</a></li>
+                    <li><a href="#" id="elLink" data-teacher="elanor">elanor</a></li>
+                </ul>
+            </nav>
 
-        <div>
-            <!-- =========================== | !!!END CONTENT WRAP | =========================== -->
+            <div class="teacherPicNav">
+                <div class="tpnPopUp"><span>Click Through Favorite Yoga Poses</span></div>
+                <a href="#" class="tpvLinkLeft">Left side</a>
+                <a href="#" class="tpvLinkRight">Right side</a>
+            </div>
 
+            <!-- =========================== | Angela | =========================== -->
+            <section class="angelaW teacher" id="angela">
+                <div class="teacherTextWrap">
+                    <img src="img/theacherPics/mAngela.jpg" class="mobileTeccherImg">
+                    <div class="ttwInner">
+                        <h2>Angela Logan</h2>
+                        <p>Angela Logan has practiced yoga for over 10 years, and has taught yoga since 2008. She received the majority of her training, including Hot Yoga, at Y+ Yoga Center, Shanghai, China.  Prior to returning to the U.S. after serving a 10 year expatriate work assignment with her husband and family, all of her teaching experience was based in Asia.  Now residing in the Columbia River Gorge, she is very enthusiastic and motivated to have the opportunity to share her international experience with fellow practitioners here in the Hood River area.</p>
+                        <a href="#" class="btn">Learn More</a>
+                        <a href="#" class="btn btnPurp Share"><i class="icon-plus"></i>Share</a>
+                    </div>
+                </div>
+            </section>
+            <!-- =========================== | !!!END Angela| =========================== -->
 
+            <!-- =========================== | Angela | =========================== -->
+            <section class="victoriaW teacher notActive" id="vic">
+                <div class="teacherTextWrap">
+                    <img src="img/theacherPics/mVic.jpg" class="mobileTeccherImg">
+                    <div class="ttwInner">
+                        <h2>Vic</h2>
+                        <p>Maecenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus. Maenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna.</p>
+                        <a href="#" class="btn">Learn More</a>
+                        <a href="#" class="btn btnPurp Share"><i class="icon-plus"></i>Share</a>
+                    </div>
+                </div>
+            </section>
+            <!-- =========================== | !!!END Angela| =========================== -->
+
+            <!-- =========================== | Angela | =========================== -->
+            <section class="chelseyW teacher notActive" id="chels">
+                <div class="teacherTextWrap">
+                    <img src="img/theacherPics/mChels.jpg" class="mobileTeccherImg">
+                    <div class="ttwInner">
+                        <h2>Chels</h2>
+                        <p>Maecenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus. Maenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna.</p>
+                        <a href="#" class="btn">Learn More</a>
+                        <a href="#" class="btn btnPurp Share"><i class="icon-plus"></i>Share</a>
+                    </div>
+                </div>
+            </section>
+            <!-- =========================== | !!!END Angela| =========================== -->
+
+            <!-- =========================== | Angela | =========================== -->
+            <section class="elanorW teacher notActive" id="elanor">
+                <div class="teacherTextWrap">
+                    <img src="img/theacherPics/mEl.jpg" class="mobileTeccherImg">
+                    <div class="ttwInner">
+                        <h2>El</h2>
+                        <p>Maecenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus. Maenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna.</p>
+                        <a href="#" class="btn">Learn More</a>
+                        <a href="#" class="btn btnPurp Share"><i class="icon-plus"></i>Share</a>
+                    </div>
+                </div>
+            </section>
+            <!-- =========================== | !!!END Angela| =========================== -->
         </div>
-        <!-- =========================== | !!!END GLOBAL PAGE WRAP | =========================== -->
+    </section>
+    <!-- =========================== | !!!END  Teacher Wrap| =========================== -->
+
+
+    <div>
+        <!-- =========================== | !!!END CONTENT WRAP | =========================== -->
+
 
     </div>
+    <!-- =========================== | !!!END GLOBAL PAGE WRAP | =========================== -->
+
+</div>
 
 
 <?php get_footer(); ?>
--- wp-content/themes/shavasana/index.php
+++ wp-content/themes/shavasana/index.php
@@ -5,18 +5,20 @@ Template Name: HOME Page
 
 get_header(); ?>
 
-<div style="width: 100%; height: 500px; background: #FF00FF;"><H1>This global hero is broken??</H1></div>
         <!-- =========================== | GLOBAL HERO WRAP | =========================== -->
-<!-- <section class="gHeroWrap homeHero fullWrap">
-
-    <!-- Global Hero | Main image / video / slider after header -->
-<!--    <section class="gHero full">
-       <div><img src="http://placehold.it/1200x550&text=Image / Video / Slider"></div>
-   </section>
-   <!-- !!!END GLOBAL HERO -->
+       <section class="gHeroWrap indexHero fullWrap" id="indexVideo">
+            <div>
+                <h2>Root Down Index</h2>
+                <p>Curabitur blandit tempus porttitor. Donec ullamcorper nulla non metus auctor fringilla. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec id elit non mi porta gravida at eget metus.</p>
+            </div>
+        </section>
+        <!-- ========================= | !!!END GLOBAL HERO WRAP | ========================= -->
 
 <!-- </section>
         <!-- ========================= | !!!END GLOBAL HERO WRAP | ========================= -->
+
+
+
 
 
 
@@ -47,23 +49,29 @@ get_header(); ?>
 
             <!-- =========================== |  | =========================== -->
             <section class="iAbout full">
-                <div class="iAboutText">
-                    <h2>About RootDownYoga</h2>
-                    <p>We at Root Down Yoga believe in living a grounded, simple life filled with vitality and health.  Our goal is to offer a simple, clean, and quiet space with invigorating, fun classes designed to suit the needs of all our members.</p>
-                    <a href="#" class="btn">Learn More</a>
-                    <div>
-                        <h2>News Letter Sign Up</h2>
-                         <form action="index.html" method="post" class="subscribe-form">
-                          <input type="email" name="email" class="subscribe-input" placeholder="Email address" autofocus>
-                          <button type="submit" class="subscribe-submit">Subscribe</button>
-                        </form>
+                <div class="iAboutInner">
+                    <div class="iAboutText">
+                        <div class="iAboutTextInner">
+                            <h2>About RootDownYoga</h2>
+                            <p>We at Root Down Yoga believe in living a grounded, simple life filled with vitality and health.  Our goal is to offer a simple, clean, and quiet space with invigorating, fun classes designed to suit the needs of all our members.</p>
+                            <a href="#" class="btn">Learn More</a>
+                            <div class="iNewsLetter">
+                                <h2>News Letter Sign Up</h2>
+                                 <form action="index.html" method="post" class="subscribe-form">
+                                  <input type="email" name="email" class="subscribeInput" placeholder="Email address" autofocus>
+                                  <input type="submit" name="subscribe" value="subscribe" class="signUpBtn iNewsBtn" />
+                                </form>
+                            </div>
+                        </div>
                     </div>
+                    <div class="iAboutClass">
+                        <h2>Yoga Classes</h2>
+                        <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. get lacinia odio sem nec elit.</p>
+                        <span class="iClassPadBtn">
+                            <a href="#" class="btn">Learn More</a>
+                        </span>
+                    <div>
                 </div>
-                <div class="iAboutClass">
-                    <h2>Yoga Classes</h2>
-                    <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. get lacinia odio sem nec elit.</p>
-                    <a href="#" class="btn">Learn More</a>
-                <div>
             </section>
             <!-- =========================== | !!!END | =========================== -->
             
@@ -71,38 +79,65 @@ get_header(); ?>
             <!-- =========================== |  | =========================== -->
             <section class="iYogaAk fulll">
                 
-                <div class="iYogaKid">
-                    <h2>Yoga for Kids</h2>
-                    <p>Maecenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus. Maenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna.</p>
-                    <a href="#" class="btn">Learn More</a>
-                    <a href="#" class="btn">View Schedule</a>
-                </div>
+                <div class="iYogaInner">
 
-                <div  class="iYogaAdult">
-                    <h2>Yoga for Adults</h2>
-                    <p>Maecenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus. Maenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna.</p>
-                    <a href="#" class="btn">Learn More</a>
-                    <a href="#" class="btn">View Schedule</a>
-                </div>
-
-                <section class="snapG">
-                    <p>Snap Goes Here</p>
-                </section>
-
-                <section class="iBlogWrap">
-                    <img height="180" width="385" style="padding-top:27px;border-width:0" onload="window.lol&amp;&amp;lol()" id="hplogo" src="/logos/doodles/2013/celia-cruzs-88th-birthday-5720827718795264-hp.jpg" alt="Celia Cruz's 88th Birthday" title="Celia Cruz's 88th Birthday">
-                    <h2>Post Name Here</h2>
-                    <p>Maecenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus. Maenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna.</p>
-                    <a href="#" class="btn">Learn More</a>
-                    <a href="#" class="btnPurp Share">Share</a>
-                </section>
-
-                <section class="iInstaWrap">
-                    <h1>Power Yoga for the Gorge</h1>
-                    <div> 
-                        igram imgs here
+                    <div class="iYogaKid">
+                        <h2>Yoga for Kids</h2>
+                        <p>Maecenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus. Maenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna.</p>
+                        <a href="#" class="btn learnMore">Learn More</a>
+                        <a href="#" class="btn">View Schedule</a>
                     </div>
-                </section>
+
+                    <div  class="iYogaAdult">
+                        <h2>Yoga for Adults</h2>
+                        <p>Maecenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus. Maenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna.</p>
+                        <a href="#" class="btn learnMore">Learn More</a>
+                        <a href="#" class="btn">View Schedule</a>
+                    </div>
+
+                    <div class="iBlog">
+
+                        <section class="iBlogWrap iFirstBlog">
+                            <img src="http://placehold.it/350x150">
+                            <div>
+                                <h2>Post Name Here</h2>
+                                <p>Maecenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus. Maenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna.</p>
+                                <a href="#" class="btn">Learn More</a>
+                                <a href="#" class="btn btnPurp Share">Share</a>
+                            </div>
+                        </section>
+
+
+                        <section class="iBlogWrap iSecondBlog">
+                            <img src="http://placehold.it/350x150">
+                            <div>
+                                <h2>Post Name Here</h2>
+                                <p>Maecenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Duis mollis, est non commodo luctus. Maenas faucibus mollis interdum. Donec sed odio dui. Praesent commodo cursus magna.</p>
+                                <a href="#" class="btn">Learn More</a>
+                                <a href="#" class="btn btnPurp Share">Share</a>
+                            </div>
+                        </section>
+
+                    </div>
+
+                    <div class="iInstagram">
+                        <section class="iInstaWrap">
+                            <h1>Power Yoga for the Gorge</h1>
+                            <div class="iGramPicWrap"> 
+                                <img src="http://placehold.it/120x120">
+                                <img src="http://placehold.it/120x120">
+                                <img src="http://placehold.it/120x120">
+                                <img src="http://placehold.it/120x120">
+                                <img src="http://placehold.it/120x120">
+                                <img src="http://placehold.it/120x120">
+                                <img src="http://placehold.it/120x120">
+                                <img src="http://placehold.it/120x120">
+                                <img src="http://placehold.it/120x120">
+                            </div>
+                        </section>
+                    </div>
+
+                </div>
 
             </section>
             <!-- =========================== | !!!END | =========================== -->
@@ -115,7 +150,6 @@ get_header(); ?>
 
     </div>
     <!-- =========================== | !!!END GLOBAL PAGE WRAP | =========================== -->
- </div>
 
 
 <?php get_footer(); ?>
